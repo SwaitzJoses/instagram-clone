3e10a1c69eeb76c01f0feb8264a45750009e1854
Swaitz Joses <swaitz.joses@gmail.com>
2021-04-23 02:10:58 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from "dotenv";
+import morgan from 'morgan'
+import colors from 'colors'
+import connectDB from './config/db.js'
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'))
+
+
+  // 🔥  ✔️  🚀
+ 
+const PORT=process.env.PORT || 5000
+app.listen(5000, () => {
+    console.log(`Hi Master! App listening on port ${PORT}!`.yellow.inverse);
+  }); 
